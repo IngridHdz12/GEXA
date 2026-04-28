fd5472e9949897aa7ef2f91a905d728c24948c1c
Osorio08 <caog0124@gmail.com>
2026-04-27 20:47:40 -0500

    "actualizacion de colores al agregar sensor"

--- lib/screens/add_sensor_screen.dart
+++ lib/screens/add_sensor_screen.dart
@@ -131,7 +131,7 @@ class _AddSensorScreenState extends State<AddSensorScreen> {
       await _gexaDevice?.disconnect();
       setState(() { _currentStep = 1; }); // Regresar al formulario si falla
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error Bluetooth: $e')),
+        SnackBar(content: Text('Error Bluetooth')),
       );
     }
   }
@@ -319,11 +319,18 @@ class _AddSensorScreenState extends State<AddSensorScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CircularProgressIndicator(color: Colors.white),
+          CircularProgressIndicator(color: Color(0xFF00698F)),
           SizedBox(height: 20),
-          Text('Vinculando dispositivo...', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+          Text(
+            'Vinculando dispositivo...', 
+            style: TextStyle(
+              color: Color(0xFF00698F), // Azul oscuro
+              fontSize: 20, 
+              fontWeight: FontWeight.bold
+            )
+          ),
           SizedBox(height: 10),
-          Text('Enviando credenciales y conectando a Firebase.\nEsto puede tardar unos segundos.', textAlign: TextAlign.center, style: TextStyle(color: Colors.white70)),
+          Text('Enviando credenciales y conectando a Firebase.\nEsto puede tardar unos segundos.', textAlign: TextAlign.center, style: TextStyle(color: Colors.black54)),
         ],
       ),
     );
